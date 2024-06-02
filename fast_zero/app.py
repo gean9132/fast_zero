311dea6d4b447c3388ae5ef283afd5748a42fab1
gean9132 <geanpepi@gmail.com>
2024-06-02 17:06:30 -0300

    Inicio do curso Fast Zero

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,25 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-
-@app.get('/')
+"""
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message, response_class=HTMLResponse)
 def read_root():
     return {'message': 'Olá mundo através do FastApi'}
+"""
+
+
+@app.get('/', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_dot():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,13 +2,21 @@ from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 
-from fast_zero.app import app 
+from fast_zero.app import app
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app) #Arrange
+    client = TestClient(app)  # Arrange
 
-    response = client.get('/') #Act
+    response = client.get('/')  # Act
 
-    assert response.status_code == HTTPStatus.OK #Assert
-    assert response.json() == {'message': 'Olá mundo através do FastApi'} #Assert
+    assert response.status_code == HTTPStatus.OK  # Assert
+    #assert response.json() == {'message': 'Olá mundo através do FastApi'}  # Assert
+
+
+def test_root_deve_retornar_ok_e_ola_mundo_em_html():
+    client = TestClient(app)
+
+    response = client.get('/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.text
